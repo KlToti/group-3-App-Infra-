198435afb6ac7e54651b84f927373b503f0c7adb
pauleentalay <pauleen.talay@cloudreach.com>
2022-10-31 17:27:11 +0100

    change security groups line, clean roles and policies

--- ec2_logstash.tf
+++ ec2_logstash.tf
@@ -48,40 +48,11 @@ resource "aws_instance" "logstash_ec2" {
         device_name = "/dev/xvdba"
         volume_size = "50"
     }
-    security_groups = [aws_security_group.logstash_sg.id]
-    iam_instance_profile = "${aws_iam_instance_profile.elk_instance_profile.name}"
+    vpc_security_group_ids = [aws_security_group.logstash_sg.id]
+    iam_instance_profile = aws_iam_instance_profile.elk_instance_profile.name
     #user_data = filebase64("${path.module}/ssm-agent-install.sh")
-}
 
-
-resource "aws_iam_instance_profile" "elk_instance_profile" {
-  name = "elk_instance_profile"
-  role = aws_iam_role.elk_role.name
-}
-
-
-resource "aws_iam_role" "elk_role" {
-  name = "elk_role"
-
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": {
-      "Effect": "Allow",
-      "Principal": {"Service": "ssm.amazonaws.com"},
-      "Action": "sts:AssumeRole"
-    }
+    tags = {
+    Name = "logstash_server"
   }
-EOF
-}
-
-
-resource "aws_iam_role_policy_attachment" "elk_attach" {
-    for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/AmazonSSMFullAccess",
-  ])
-
-  role       = aws_iam_role.elk_role.name
-  policy_arn = each.value
 }
