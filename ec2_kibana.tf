c9a261ec9948fe52f681bb439634e5cbaf466662
Yelizaveta Ivanchenko <yelizaveta.ivanchenko@cloudreach.com>
2022-11-08 14:33:47 +0100

    changed to rood block device

--- ec2_kibana.tf
+++ ec2_kibana.tf
@@ -2,14 +2,15 @@ resource "aws_instance" "kibana_server" {
   ami = data.aws_ami.kibana_on_ubuntu.id
   instance_type = "t3.medium"
   subnet_id = data.aws_subnet.public_subnet.id
+  root_block_device  {
+        #device_name = "elasticSearch-ebs"
+        volume_size = "50"
+    }
 user_data = templatefile("${path.module}/user-data.sh.tpl",
   {
     elasticsearch_ip = aws_instance.elasticSearch_ec2.private_ip
   })
-  ebs_block_device  {
-    device_name = "/dev/xvdba"
-    volume_size = "50"
-    }
+  
   vpc_security_group_ids = [aws_security_group.kibana_sg.id]
   iam_instance_profile = aws_iam_instance_profile.elk_instance_profile.name
   tags = {
